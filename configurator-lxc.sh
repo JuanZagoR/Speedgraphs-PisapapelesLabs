3c8eca7885173428efbe7417ec6a1d31f995274b
Juan Zago Rodríguez <36072973+JuanZagoR@users.noreply.github.com>
2023-07-16 15:45:48 -0600

    Minor fixes

--- configurator-lxc.sh
+++ configurator-lxc.sh
@@ -1,57 +1,57 @@
 #!/bin/sh
 set -e
-echo -e "\e[1;35mSpeedgraphs - Pisapapeles Labs\e[0m"
+echo -e "\e[0;35mSpeedgraphs - Pisapapeles Labs\e[0m"
 sleep 2
 
-echo -e "\e[0;32mComprobando permisos...\e[0m"
+echo -e "\e[1;36mComprobando permisos...\e[0m"
 if [[ $EUID -ne 0 ]]; then
    echo -e "\e[0;31mPara usar este script, es necesario elevar permisos. Use 'sudo bash "$0"' en su lugar\e[0m" 1>&2
    exit 1
 fi
 
-echo -e "\e[0;32mActualizando repositorios...\e[1;30m"
+echo -e "\e[0;32m➤ Actualizando repositorios...\e[1;30m"
 sleep 2
-sudo apt update && echo "Repositorios actualizados" || echo -e "\e[0;31mHubo un problema al actualizar repositorios\e[0m"
+sudo apt update && echo -e "\e[0;32m✔ Repositorios actualizados\e[0m" || echo -e "\e[0;31m✖ Hubo un problema al actualizar repositorios\e[0m"
 
 sleep 2
-echo -e "\e[0;32mLos siguientes paquetes serán actualizados:\e[0m"
+echo -e "\e[0;32m➤ Los siguientes paquetes serán actualizados:\e[0m"
 
 sleep 2
 sudo apt list --upgradable
 
 sleep 5
-echo -e "\e[0;32mActualizando paquetes...\e[0m"
+echo -e "\e[0;32m➤ Actualizando paquetes...\e[1;30m"
 
 sleep 2
-sudo apt upgrade -y && echo -e "\e[0;32mPaquetes actualizados\e[0m" || echo -e "\e[0;31mHubo un problema al actualizar los paquetes\e[0m"
-echo -e "\e[0;32mInstalando paquetes pendientes...\e[0m"
+sudo apt upgrade -y && echo -e "\e[0;32m✔ Paquetes actualizados\e[0m" || echo -e "\e[0;31m✖ Hubo un problema al actualizar los paquetes\e[0m"
+echo -e "\e[0;32m➤ Instalando paquetes pendientes...\e[0m"
 
 sleep 2
-sudo apt install sudo net-tools wget curl -y && echo -e "\e[0;32mDependencias instaladas correctamente\e[0m" || echo -e "\e[0;31mHubo un problema al instalar dependencias\e[0m"
+sudo apt install sudo net-tools wget curl -y && echo -e "\e[0;32m✔ Dependencias instaladas correctamente\e[0m" || echo -e "\e[0;31m✖ Hubo un problema al instalar dependencias\e[0m"
 
 sleep 2
-echo -e "\e[0;32mInstalando la última versión de Docker (Community Edition) desde el sitio oficial...\e[0m"
+echo -e "\e[0;32m➤ Instalando la última versión de Docker (Community Edition) desde el sitio oficial...\e[1;30m"
 sudo curl -fsSL https://get.docker.com/ | sudo bash -s
-echo -e "\e[0;32mDocker-CE ha sido instalado\e[0m" || echo -e "\e[0;31mError al instalar Docker-CE\e[0m"
+echo -e "\e[0;32m✔ Docker-CE ha sido instalado\e[0m" || echo -e "\e[0;31m✖ Error al instalar Docker-CE\e[0m"
 
 sleep 2
-echo -e "\e[0;32mCreando carpetas y descargando imagen de Home Assistant\e[0m"
+echo -e "\e[0;32m➤ Creando carpetas y descargando imagen de Home Assistant\e[0m"
 sudo mkdir -p /mnt/Docker/Speedgraphs
 sudo chmod 777 -R /mnt/Docker/Speedgraphs
-cd /mnt/Docker/Speedgraphs && echo -e "\e[0;32mDirectorios creados\e[0m" || echo -e "\e[0;31mError al acceder al directorio\e[0m"
-echo -e "\e[0;32mObteniendo la última imagen de Home Assistant, desde GHCR...\e[0m"
+cd /mnt/Docker/Speedgraphs && echo -e "\e[0;32m✔ Directorios creados\e[0m" || echo -e "\e[0;31m✖ Error al acceder al directorio\e[0m"
+echo -e "\e[0;32m➤ Obteniendo la última imagen de Home Assistant, desde GHCR...\e[1;30m"
 
 sleep 2
-docker pull ghcr.io/home-assistant/home-assistant:stable && echo -e "\e[0;32mImagen descargada correctamente\e[0m" || echo -e "\e[0;31mHubo un problema al descargar la imagen\e[0m"
-echo -e "\e[0;32mObteniendo plantilla de Docker-Compose desde repositorio\e[0m"
+docker pull ghcr.io/home-assistant/home-assistant:stable && echo -e "\e[0;32m✔ Imagen descargada correctamente\e[0m" || echo -e "\e[0;31m✖ Hubo un problema al descargar la imagen\e[0m"
+echo -e "\e[0;32m➤ Obteniendo plantilla de Docker-Compose desde repositorio\e[0m"
 
 sleep 2
-wget https://raw.githubusercontent.com/JuanZagoR/Speedgraphs-PisapapelesLabs/dev/Docker-Compose%20Templates/HomeAssistant/docker-compose.yaml && echo -e "\e[0;32mPlantilla  descargada correctamente\e[0m" || echo -e "\e[0;31mHubo un problema al descargar la plantilla\e[0m"
+wget -N https://raw.githubusercontent.com/JuanZagoR/Speedgraphs-PisapapelesLabs/dev/Docker-Compose%20Templates/HomeAssistant/docker-compose.yaml -O docker-compose.yaml && echo -e "\e[0;32m✔ Plantilla descargada correctamente\e[0m" || echo -e "\e[0;31m✖ Hubo un problema al descargar la plantilla\e[0m"
 
 sleep 2
-echo -e "\e[0;32mCreando contenedor a partir de plantilla...\e[0m"
-docker compose up -d && echo -e "\e[0;32mContenedor creado, revisando estado...\e[0m" || echo -e "\e[0;31mHubo un problema al crear el contenedor\e[0m"
+echo -e "\e[0;32m➤ Creando contenedor a partir de plantilla...\e[1;30m"
+docker compose up -d && echo -e "\e[0;32m✔ Contenedor creado, revisando estado...\e[0m" || echo -e "\e[0;31m✖ Hubo un problema al crear el contenedor\e[0m"
 
 sleep 5
 docker logs PPLabs-HomeAssistant-Prod
-echo -e "\e[1;34mConfigura la instancia de Home Assistant de este servidor, a través de http://$(hostname  -I | cut -f1 -d' '):8123\e[0m"
+echo -e "\e[1;34m➤ Configura la instancia de Home Assistant de este servidor, a través de http://$(hostname  -I | cut -f1 -d' '):8123\e[0m"
