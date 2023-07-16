296b378d29a3528a775f7fedd1abf61c35d3e5cd
Juan Zago Rodríguez <36072973+JuanZagoR@users.noreply.github.com>
2023-07-16 16:17:30 -0600

    Minor corrections

--- configurator-lxc.sh
+++ configurator-lxc.sh
@@ -9,47 +9,47 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-echo -e "\e[0;32m➤ Actualizando repositorios...\e[1;30m"
+echo -e "\e[1;36m➤ Actualizando repositorios...\e[1;30m"
 sleep 2
 sudo apt update && echo -e "\e[0;32m✔ Repositorios actualizados\e[0m" || echo -e "\e[0;31m✖ Hubo un problema al actualizar repositorios\e[0m"
 
 sleep 2
-echo -e "\e[0;32m➤ Los siguientes paquetes serán actualizados:\e[0m"
+echo -e "\e[1;36m➤ Los siguientes paquetes serán actualizados:\e[0m"
 
 sleep 2
 sudo apt list --upgradable
 
 sleep 5
-echo -e "\e[0;32m➤ Actualizando paquetes...\e[1;30m"
+echo -e "\e[1;36m➤ Actualizando paquetes...\e[1;30m"
 
 sleep 2
 sudo apt upgrade -y && echo -e "\e[0;32m✔ Paquetes actualizados\e[0m" || echo -e "\e[0;31m✖ Hubo un problema al actualizar los paquetes\e[0m"
-echo -e "\e[0;32m➤ Instalando paquetes pendientes...\e[0m"
+echo -e "\e[1;36m➤ Instalando dependencias faltantes...\e[0m"
 
 sleep 2
 sudo apt install sudo net-tools wget curl -y && echo -e "\e[0;32m✔ Dependencias instaladas correctamente\e[0m" || echo -e "\e[0;31m✖ Hubo un problema al instalar dependencias\e[0m"
 
 sleep 2
-echo -e "\e[0;32m➤ Instalando la última versión de Docker (Community Edition) desde el sitio oficial...\e[1;30m"
+echo -e "\e[1;36m➤ Instalando la última versión de Docker (Community Edition) desde el sitio oficial...\e[1;30m"
 sudo curl -fsSL https://get.docker.com/ | sudo bash -s
 echo -e "\e[0;32m✔ Docker-CE ha sido instalado\e[0m" || echo -e "\e[0;31m✖ Error al instalar Docker-CE\e[0m"
 
 sleep 2
-echo -e "\e[0;32m➤ Creando carpetas y descargando imagen de Home Assistant\e[0m"
+echo -e "\e[1;36m➤ Creando carpetas y descargando imagen de Home Assistant\e[0m"
 sudo mkdir -p /mnt/Docker/Speedgraphs
 sudo chmod 777 -R /mnt/Docker/Speedgraphs
 cd /mnt/Docker/Speedgraphs && echo -e "\e[0;32m✔ Directorios creados\e[0m" || echo -e "\e[0;31m✖ Error al acceder al directorio\e[0m"
-echo -e "\e[0;32m➤ Obteniendo la última imagen de Home Assistant, desde GHCR...\e[1;30m"
+echo -e "\e[1;36m➤ Obteniendo la última imagen de Home Assistant, desde GHCR...\e[1;30m"
 
 sleep 2
 docker pull ghcr.io/home-assistant/home-assistant:stable && echo -e "\e[0;32m✔ Imagen descargada correctamente\e[0m" || echo -e "\e[0;31m✖ Hubo un problema al descargar la imagen\e[0m"
-echo -e "\e[0;32m➤ Obteniendo plantilla de Docker-Compose desde repositorio\e[0m"
+echo -e "\e[1;36m➤ Obteniendo plantilla de Docker-Compose desde repositorio\e[0m"
 
 sleep 2
-wget -N https://raw.githubusercontent.com/JuanZagoR/Speedgraphs-PisapapelesLabs/dev/Docker-Compose%20Templates/HomeAssistant/docker-compose.yaml -O docker-compose.yaml && echo -e "\e[0;32m✔ Plantilla descargada correctamente\e[0m" || echo -e "\e[0;31m✖ Hubo un problema al descargar la plantilla\e[0m"
+wget https://raw.githubusercontent.com/JuanZagoR/Speedgraphs-PisapapelesLabs/dev/Docker-Compose%20Templates/HomeAssistant/docker-compose.yaml -O docker-compose.yaml && echo -e "\e[0;32m✔ Plantilla descargada correctamente\e[0m" || echo -e "\e[0;31m✖ Hubo un problema al descargar la plantilla\e[0m"
 
 sleep 2
-echo -e "\e[0;32m➤ Creando contenedor a partir de plantilla...\e[1;30m"
+echo -e "\e[1;36m➤ Creando contenedor a partir de plantilla...\e[1;30m"
 docker compose up -d && echo -e "\e[0;32m✔ Contenedor creado, revisando estado...\e[0m" || echo -e "\e[0;31m✖ Hubo un problema al crear el contenedor\e[0m"
 
 sleep 5
