9fd21669e8dc5712c9937d31b8b7b829a3b1161d
Juan Zago Rodríguez <36072973+JuanZagoR@users.noreply.github.com>
2023-07-16 22:16:04 -0600

    6th fix for line 79

--- configurator-lxc.sh
+++ configurator-lxc.sh
@@ -78,8 +78,7 @@ echo -e "\e[1;34m  para añadir las integraciones necesarias para llevar a cabo 
 
 sleep 5
 echo
-echo "Presiona cualquier tecla para continuar con el script:"
-read -n 1 -p ⠀
+read -n 1 -p "Presiona cualquier tecla para continuar con el script:"
    echo -e "\e[0;32mContinuando con la configuración...\e[0m"
 
 
