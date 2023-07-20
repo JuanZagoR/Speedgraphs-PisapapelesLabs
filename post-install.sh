75742494ea5501fffbc07ab484e31efee18ca71f
Juan Zago Rodríguez <36072973+JuanZagoR@users.noreply.github.com>
2023-07-20 17:24:31 -0600

    Minor changes

--- post-install.sh
+++ post-install.sh
@@ -4,6 +4,7 @@ echo -e "\e[1;35mSpeedgraphs - Pisapapeles Labs\e[0m"
 sleep 1
 echo -e "\e[0;35mPost-Install 0.1 - Alpha\e[0m"
 sleep 2
+echo
 
 echo -e "\e[1;36m➤ Eliminando respaldo antiguo existente...\e[0m"
 sleep 1
@@ -15,8 +16,8 @@ sleep 1
 sudo mkdir /mnt/Docker/Speedgraphs/Config/BackupConfig
 yes | sudo cp -i /mnt/Docker/Speedgraphs/Config/*.yaml /mnt/Docker/Speedgraphs/Config/BackupConfig
 echo -e "\e[0;32m✔ Configuración anterior respaldada \e[0m"
-echo
 
+echo
 echo -e "\e[1;36m➤ Obteniendo archivos de configuración personalizados para Home Assistant\e[0m"
 cd /mnt/Docker/Speedgraphs/Config
 sudo wget -q https://github.com/JuanZagoR/Speedgraphs-PisapapelesLabs/blob/dev/HomeAssistant%20Config/configuration.yaml -O configuration.yaml && echo -e "\e[0;32m✔ Archivo de configuración reemplazado \e[0m" || echo -e "\e[0;31m✖ Hubo un problema al reemplazar el archivo\e[0m"
@@ -26,4 +27,5 @@ sudo wget -q https://github.com/JuanZagoR/Speedgraphs-PisapapelesLabs/blob/dev/H
 sudo wget -q https://github.com/JuanZagoR/Speedgraphs-PisapapelesLabs/blob/dev/HomeAssistant%20Config/iperf3.yaml -O iperf3.yaml && echo -e "\e[0;32m✔ Configuración de iPerf3 añadida \e[0m" || echo -e "\e[0;31m✖ Hubo un problema al añadir la configuración de iPerf3\e[0m"
 
 echo
-echo -e "\e[1;36mConsulta la documentación para terminar la configuración. Gracias por usar este script.\e[0m"
+echo -e "\e[1;33mConsulta la documentación oficial en github.com/JuanZagoR/Speedgraphs-PisapapelesLabs para terminar la configuración.\e[0m"
+echo -e "\e[1;36mGracias por usar este script.\e[0m"
